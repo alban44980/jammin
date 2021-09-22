e6b47115943c8f7159f9f03447c5100828474c4a
Alban Mansord <alban.mansord@gmail.com>
2021-09-22 17:11:06 +0200

    createjam form

--- client/jammin/src/components/CreateJam.js
+++ client/jammin/src/components/CreateJam.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const initialState = {
+  date: '',
+  description: '',
+  location: '',
+  coordinates: [],
+  host: '',
+  numofParticipants : 0,
+  languages: [],
+  pastEvent: undefined,
+  comingEvent: undefined
+}
 
 function CreateJam() {
+
+  const [state, setState] = useState(initialState);
+
+
+  function handleChange () {
+
+  }
+
   return (
-    <div>
-      <h1>I AM THE CREATE A JAM ROUTE</h1>
+    <div className="createJam-main">
+      <form className="jam-form">
+        <h1>CREATE YOUR JAM 🥳🥳🥳 </h1>
+        <input type="datetime-local"
+        placeholder = "Date"
+        name = "date"
+        value={state.date}
+        onChange={handleChange}
+        className="event-input"
+        />
+        <input type="text"
+        placeholder = "Location"
+        name = "location"
+        value={state.location}
+        onChange={handleChange}
+        className="event-input"
+        />
+        <input type="text"
+        placeholder = "Description"
+        name = "description"
+        value={state.description}
+        onChange={handleChange}
+        className="event-input"
+        />
+
+      </form>
     </div>
   )
 }
