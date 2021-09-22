9f8540df1b58575da8468a026b60f8a7e0e32037
Alban Mansord <alban.mansord@gmail.com>
2021-09-22 18:34:01 +0200

    adjusted findjam form

--- client/jammin/src/components/FindJam.js
+++ client/jammin/src/components/FindJam.js
@@ -3,9 +3,10 @@ import React from 'react'
 function FindJam() {
   return (
     <div className="findJam-main">
-      <form action="">
+      <form className="find-form" action="">
       <h1 id="find-jam">👇👇 Enter your location to find a jam in your city 👇👇</h1>
       <input type="text" placeholder="city"/>
+      <button className="find-btn">Search</button>
       </form>
     </div>
   )
