2a06905ac54dbdbb83fdf9de483cb59429f5dd7f
Alban Mansord <alban.mansord@gmail.com>
2021-09-26 21:33:03 +0200

    added css on signup page

--- client/jammin/src/components/Signup/SignUp.jsx
+++ client/jammin/src/components/Signup/SignUp.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import './signup.css';
 
 function SignUp() {
-  return <div>I AM THE SIGNUP COMPONENT</div>;
+  return (
+    <div className="signup-container">
+      <h1>Create your account</h1>
+      <form className="signup-form">
+        <input
+          required
+          id="firstname"
+          type="text"
+          placeholder="First name"
+          name="firstname"
+        />
+        <input
+          required
+          id="lastname"
+          type="text"
+          placeholder="Last name"
+          name="lastname"
+        />
+        <input
+          required
+          id="email"
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+        <input
+          required
+          id="password"
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <button>JOIN NOW</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
+
+//firstname LastName Email Password RepeatPassword
