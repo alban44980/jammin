4cb29d6c373f3e75fb018b2d6da12a74bcf6c61f
Alban Mansord <alban.mansord@gmail.com>
2021-09-26 20:45:29 +0200

    added css for login page

--- client/jammin/src/components/LogIn/LogIn.jsx
+++ client/jammin/src/components/LogIn/LogIn.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import './login.css';
 
 function LogIn() {
-  return <div>I AM THE LOGIN COMPONENT</div>;
+  return (
+    <div className="login-container">
+      <form className="login-form">
+        <input
+          required
+          id="email"
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+        <input
+          required
+          id="password"
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <button>LOG IN</button>
+      </form>
+    </div>
+  );
 }
 
 export default LogIn;
